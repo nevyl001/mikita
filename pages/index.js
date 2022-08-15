35599b39fc2ccae11e76ef26b518fb1ba7b798ae
Nevyl <christiannevyl@MacBook-Pro-de-ChristianNevyl.local>
2022-08-15 16:49:16 -0500

    changes15august

--- pages/index.js
+++ pages/index.js
@@ -79,6 +79,270 @@ export default function Home() {
             })}
           </Carousel>
         </div>
+        <div className={styles.porque}>
+          <div className={styles.porque_left}>
+            <Subtitle size="medium_1" color="yellow">
+              ¿PORQUÉ MIKITA TOWN?
+            </Subtitle>
+            <Paragraph>
+              MIKITA TOWN SE UBICA EN UNA ZONA PRVILEGIADA QUE PERMITE TRASLADOS
+              EN CORTO TIEMPO, GRACIAS AL POCO TRAFICO VEHICULAR.
+            </Paragraph>
+            <Paragraph>
+              SE ENCUENTRA A SOLO 10 MINUTOS DE ZONA COMERCIAL CON ALTA DEMANDA,
+              ESCUELAS, SUPERMERCADOS Y HOSPITALES.
+            </Paragraph>
+            <Paragraph>LOCALIZADO EN SAN ISIDRO JURIQUILLA, QRO.</Paragraph>
+            <Paragraph>
+              UNA ZONA EMERGENTE QUE GARANTIZA EL RETORNO PARA LOS
+              INVERSIONISTAS Y UN PATRIMONIO SÓLIDO PARA CUALQUIER FAMILIA.
+            </Paragraph>
+            <Paragraph>
+              EL DESARROLLO SE DEFERENCÍA POR SU PROPUESTA INNOVADORA EN
+              ARQUITECTURA, DESTACANDO SU DISEÑO MINIMALISTA, ESPACIOS AMPLIOS E
+              ILUMINADOS Y ACABADOS ÚNICOS DE LARGA VIDA.
+            </Paragraph>
+            <Paragraph>
+              EL CONJUNTO DE AMENIDADES Y ATRIBUTOS DE CADA MODELO ENTRE CASA Y
+              DEPA, FACILITAN EL ESTILO DE VIDA DE LAS PERSONAS, CONSIGUENDO
+              ASÍ, ESTABILIDAD Y UN ENTORNO EN ARMONÍA.
+            </Paragraph>
+          </div>
+          <div className={styles.porque_right}>
+            <div className={styles.imagen}>
+              <Image
+                src="/imgs/imright.png"
+                alt="im-video"
+                width={1920}
+                height={1080}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.porque_down}>
+          <div className={styles.text_1}>
+            <Title size="small" color="white">
+              122 FAMILIAS
+            </Title>
+            <Subtitle size="small" color="yellow">
+              SATISFECHAS
+            </Subtitle>
+          </div>
+          <div className={styles.text_2}>
+            <div className={styles.text_in}>
+              <Title size="small" color="white">
+                34 DUPLEX
+              </Title>
+            </div>
+            <div className={styles.text_in}>
+              <Title size="small" color="white">
+                70 CASAS
+              </Title>
+            </div>
+            <div className={styles.text_in}>
+              <Title size="small" color="white">
+                18 DEPAS
+              </Title>
+            </div>
+            <div className={styles.text_in_2}>
+              <Subtitle size="medium" color="yellow">
+                ESCRITURADOS
+              </Subtitle>
+            </div>
+          </div>
+        </div>
+        <div className={styles.btn_porque}>
+          <div className={styles.btn}>
+            <Link href="/">
+              <a>QUIERO MÁS INFORMACIÓN</a>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.elige_title}>
+          <Title size="medium" color="white">
+            ELIGE TU ESTILO
+          </Title>
+        </div>
+        <div className={styles.elige}>
+          <div className={styles.elige_left}>
+            <div className={styles.imagen}>
+              <Image
+                src="/imgs/mhouse.jpg"
+                alt="im-house"
+                width={4056}
+                height={3040}
+              />
+              <div className={styles.top_content}>
+                <div className={styles.top_text1}>ÁREA PLUS</div>
+                <div className={styles.top_text2}>$2,490,000</div>
+              </div>
+              <div className={styles.bottom_content}>
+                <div className={styles.top_text1}>COCINA MEGA</div>
+                <div className={styles.top_text2}>$2,490,000</div>
+              </div>
+            </div>
+            <div className={styles.text}>
+              <Title size="medium" color="white">
+                MIKITA HOUSE
+              </Title>
+              <Paragraph>CONCEPTO MINIMALISTA, ROBUSTO Y ELEGANTE.</Paragraph>
+              <Paragraph>
+                SE ADAPTA AL ESTILO DE VIDA DE TU FAMILIA , YA QUE, PUEDES
+                ELEGIR "ÁREA PLUS" PARA USARLO COMO CUARTO DE ESTUDIO, HOMME
+                OFFICE, ETC, O "COCINA MEGA" CON BARRA AL CENTRO, TIPO ISLA.
+              </Paragraph>
+            </div>
+            <div className={styles.btn}>
+              <Link href="/">
+                <a>SABER MÁS</a>
+              </Link>
+            </div>
+          </div>
+          <div className={styles.elige_right}>
+            <div className={styles.imagen}>
+              <Image
+                src="/imgs/dabu.jpg"
+                alt="im-house"
+                width={4056}
+                height={3040}
+              />
+              <div className={styles.top_content2}>
+                <div className={styles.top_text1}>DOBLE ALTURA</div>
+                <div className={styles.top_text2}>$2,190,000</div>
+              </div>
+              <div className={styles.bottom_content2}>
+                <div className={styles.top_text1}>1 PISO</div>
+                <div className={styles.top_text2}>$1,690,000</div>
+              </div>
+            </div>
+            <div className={styles.text}>
+              <Title size="medium" color="white">
+                DABURÚ
+              </Title>
+              <Paragraph>CONCEPTO MINIMALISTA Y MUY AMPLIO.</Paragraph>
+              <Paragraph>
+                TE BRINDA DOS EXCELENTES OPCIONES, YA QUE, PUEDES ELEGIR "UN
+                PISO" CON AMPLIO ESPACIO PARA JARDÍN Y TERRAZA O "DOBLE ALTURA"
+                CON AMPLIA TERRAZA. IDEAL PARA INVERSIÓN CON RETORNO O
+                PATRIMONIAL.
+              </Paragraph>
+            </div>
+            <div className={styles.btn}>
+              <Link href="/">
+                <a>SABER MÁS</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.video}>
+          <div className={styles.imagen}>
+            <Image
+              src="/imgs/imright.png"
+              alt="im-video"
+              width={1920}
+              height={1080}
+            />
+          </div>
+          <div className={styles.video_text}>
+            <div className={styles.btn}>
+              <Link href="/">
+                <a>QUIERO HABLAR CON UN ASESOR</a>
+              </Link>
+            </div>
+            <div className={styles.text}>
+              <Title size="small" color="white">
+                ESTAS A PUNTO DE EMPEZAR UN NUEVO ESTILO DE VIDA.
+              </Title>
+            </div>
+          </div>
+        </div>
+        <div className={styles.town}>
+          <div className={styles.town_text}>
+            <Title size="medium" color="white">
+              MIKITA TOWN
+            </Title>
+            <Subtitle size="medium" color="white">
+              EMPIEZA UN NUEVO ESTILO DE VIDA
+            </Subtitle>
+          </div>
+        </div>
+        <div className={styles.images_box}>
+          <div
+            className={styles.image_item1}
+            style={{
+              backgroundImage: `url(/imgs/pici.jpeg)`,
+            }}
+          ></div>
+          <div
+            className={styles.image_item2}
+            style={{
+              backgroundImage: `url(/imgs/gym.jpeg)`,
+            }}
+          ></div>
+        </div>
+
+        <div className={styles.images_box1}>
+          <div className={styles.box_left}>
+            <div className={styles.images_box_1}>
+              <div
+                className={styles.image_item_1}
+                style={{
+                  backgroundImage: `url(/imgs/bar1.jpeg)`,
+                }}
+              ></div>
+            </div>
+            <div className={styles.images_box_2}>
+              <div
+                className={styles.image_item_2}
+                style={{
+                  backgroundImage: `url(/imgs/gym1.jpeg)`,
+                }}
+              ></div>
+              <div
+                className={styles.image_item_2}
+                style={{
+                  backgroundImage: `url(/imgs/gym2.jpeg)`,
+                }}
+              ></div>
+            </div>
+          </div>
+          <div className={styles.box_rigth}>
+            <div
+              className={styles.image_item_1}
+              style={{
+                backgroundImage: `url(/imgs/bar2.jpeg)`,
+              }}
+            ></div>
+          </div>
+        </div>
+
+        <div className={styles.images_box2}>
+          <div className={styles.box_left}>
+            <div className={styles.images_box_2}>
+              <div
+                className={styles.image_item_2}
+                style={{
+                  backgroundImage: `url(/imgs/pici1.jpeg)`,
+                }}
+              ></div>
+              <div
+                className={styles.image_item_2}
+                style={{
+                  backgroundImage: `url(/imgs/sillon.jpeg)`,
+                }}
+              ></div>
+            </div>
+            <div className={styles.images_box_1}>
+              <div
+                className={styles.image_item_1}
+                style={{
+                  backgroundImage: `url(/imgs/balcon.png)`,
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
