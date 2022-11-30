23e727bc648f0c504eab0c9ccf0f20418111950c
Nevyl <christiannevyl@MacBook-Pro-de-ChristianNevyl.local>
2022-11-30 13:35:14 -0600

    changeslast

--- pages/casas.js
+++ pages/casas.js
@@ -628,21 +628,23 @@ export default function casas() {
             VISÍTANOS
           </Paragraph>
 
-          <div className={styles.arrow1}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="50"
-              height="50"
-              fill="#ffcf00"
-              className="bi bi-arrow-down"
-              viewBox="0 0 16 16"
-            >
-              <path
-                ffillRule="evenodd"
-                d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-              />
-            </svg>
-          </div>
+          <Link href="#contacto">
+            <div className={styles.arrow1}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="50"
+                height="50"
+                fill="#ffcf00"
+                className="bi bi-arrow-down"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  ffillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                />
+              </svg>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
